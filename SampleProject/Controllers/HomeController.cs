339b713423c95993e721e6672a199ba7a0dbc404
Richard Slater <github@richard-slater.co.uk>
2012-09-02 21:00:21 +0100

    Added testbed code
    Testbed is comprised of:
    * 2 properties in web.config
    * Transforms of 1 property for Debug and Release
    * Controller and View to display properties
    * Markup and Stylings to make it usable
    * Publish configuration to deploy to local IIS

---
+++ SampleProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace SampleProject.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("Per Build Config Setting", ConfigurationManager.AppSettings["PerBuildConfig"]);
+            dictionary.Add("Per Environment Setting", ConfigurationManager.AppSettings["PerEnvironment"]);
+            return View(dictionary);
+        }
+    }
+}
